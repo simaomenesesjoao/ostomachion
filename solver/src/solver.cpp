6bff4fd6e036b581aef40f243925611dcbacf74c
simao <simao_meneses_joao@hotmail.com>
2025-04-16 00:56:33 +0100

    refactored get_next_states

--- solver/src/solver.cpp
+++ solver/src/solver.cpp
@@ -10,69 +10,33 @@
 #include "settings.hpp"
 
 std::vector<std::shared_ptr<State>> find_next_states(
-    const State& state, const CalcSettings& settings, int thread_register){
-
-    std::vector<std::shared_ptr<State>> next_states;
+    const State& state, void* overlaps, void* node_selector){
+    
     if(!state.is_valid() or state.is_complete())
         return {};
     
-    unsigned int node_index = Selector<Num>::select(*current_polygon);
-    Nod& obtusest_node = current_polygon->ll_node_from_index(node_index)->data;
+    const auto& frame = state.get_frame();
+    unsigned int node_index = node_selector(frame);
+    Nod& insertion_node = node_from_index(node_index)->data;
 
-    // Find which polygons haven't been used yet
-    for(unsigned int i: used_polys->find_available_types()){
+    std::vector<std::shared_ptr<State>> next_states;
+    for(const auto& restricted_poly: state.get_remaining_poly_pool()){
 
-        for(auto reflected: std::vector<bool>{true, false}){
+        for(const auto& transformed_poly: restricted_poly.variations){
+            if(angles_compatible(transformed_poly.head.angle_opening, insertion_node.angle_opening)){
+                auto new_poly = transformed_poly.move_into(insertion_node);
 
-            Poly poly = used_polys->get_prepoly_at(i);
+                if(overlaps(new_poly, restricted_poly.prison) and !overlaps(new_poly, frame)){
+                    std::shared_ptr<State> new_state = std::make_shared<State>(state);
+                    new_state->insert_polygon(node_index, new_poly);
+                    next_states.push_back(new_state);
+                }
 
-            if(not allow_reflection and reflected){
-                continue;
-            }
-                
-            if(reflected){
-                poly.flip_x();
-            } 
-
-            LL_Node<Nod> *current = poly.head;
-            for(unsigned j = 0; j < poly.size_ll; j++){
-                Nod& proposed_node = current->data;
-                current = current->next;
-                
-                if(angles_compatible(proposed_node.angle_opening, obtusest_node.angle_opening)){
-                    poly.translate(obtusest_node.position - proposed_node.position);
-                    poly.rotate(obtusest_node.angle_start - proposed_node.angle_end, 
-                                obtusest_node.position);
-                    
-                    // if(not current_polygon->overlaps(poly))){
-                    if(not overlaps->operator()(poly, *current_polygon, used_polys->get_all_polys())){
-
-                        if(i==0 and !poly.convex_overlaps(used_polys->get_irreducible_section())){                            
-                            continue;
-                        }
-                        // Posso ter um merge que não canibalize o outro polígono - rvalue refs
-                        Poly new_frame{*current_polygon}; 
-                        Poly new_poly{poly};
-
-                        LL_Node<Nod> *node_frame = new_frame.ll_node_from_index(node_index);
-                        LL_Node<Nod> *node_poly = new_poly.ll_node_from_index(j);
-
-                        new_frame.merge(node_frame, new_poly, node_poly);
-                        new_frame.prune_LL({node_frame, node_poly}, Getter(), stream);
-                        
-                        Inner new_used_polys = *used_polys;
-                        new_used_polys.insert(i, poly);
-                        
-                        next_states.push_back(std::make_shared<State>(new_frame, new_used_polys, overlaps));
-
-                    }
-                }   
             }
         }
     }
-
     return next_states;
-    
+
 }
 
 std::pair<std::vector<std::shared_ptr<State>>, Analytics> get_combinations(
@@ -94,7 +58,7 @@ std::pair<std::vector<std::shared_ptr<State>>, Analytics> get_combinations(
                 auto state = container->pop();
                 if(!state)
                     break;
-                auto next_states = find_next_states(**state, settings, 1);
+                auto next_states = find_next_states(**state, settings.overlapper, settings.node_selector);
                 container->insert(next_states);
             }
             std::cout << "thread finishing\n";
